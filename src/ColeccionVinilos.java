f388f559898b2cec2d167835624dcba699a3ecaa
Natur <a.rivas06@ufromail.cl>
2024-03-14 22:09:55 -0300

    Agrega comentarios a todo el codigo y cambia el formateo de los metodos de ColeccionVinilos.java
    Signed-off-by: Natur <a.rivas06@ufromail.cl>

--- src/ColeccionVinilos.java
+++ src/ColeccionVinilos.java
@@ -2,63 +2,75 @@ import java.util.ArrayList;
 
 public class ColeccionVinilos{
 
+    // Lista dinamica con las instancias de tipo Vinilo
     ArrayList<Vinilo> vinilos = new ArrayList<>();
 
+    // Agrega un vinilo a la lista vinilos con sus respectivos datos
     public void agregarVinilo(String artista, String album, String year){
 
+        // Crea una instancia de un Vinilo
         Vinilo vinilo = new Vinilo();
+
+        // Inserta los datos
         vinilo.artista = artista;
         vinilo.album = album;
         vinilo.year = year;
 
+        // Si hay espacio lo inserta
         if(vinilos.size() <= 100) {
             vinilos.add(vinilo);
+        //Sino no inserta y alerta que la colección ya esta completa
         }else{
             System.out.println("La colección ya tiene 100 espacios %n");
         }
 
     }
 
-    public int totalVinilos(){
-        return vinilos.size();
-    }
+    // Retorna el total de vinilos
+    public int totalVinilos(){return vinilos.size();}
 
-    public void mostrarTotal(){
-        System.out.printf("La colección tiene un total de %s vinilos %n%n", totalVinilos());
-    }
+    // Mustra por pantalla la cantidad de vinilos de la colección
+    public void mostrarTotal(){ System.out.printf("La colección tiene un total de %s vinilos %n%n", totalVinilos()); }
 
+    /*
+    * Se le ingresa un artista e indica si este es autor de almenos un vinilo de la colección, además retorna
+    * true o false según el caso
+    */
     public boolean buscarArtista(String artista){
-        for(Vinilo vinilo : vinilos)
-            if (vinilo.artista == artista)
+        for(Vinilo vinilo : vinilos) {
+            if (vinilo.artista.toLowerCase().equals(artista.toLowerCase())){
+                System.out.printf("El artista %s se encuentra en la colección%n%n", artista);
                 return true;
+            }
+        }
+
+        System.out.printf("El artista %s no existe en la colección%n%n", artista);
         return false;
     }
 
+    // Se le indica un artista e imprime todos los vinilos de este
     public void mostrarBusquedaArtista(String artista){
         int i = 1;
         for(Vinilo vinilo : vinilos) {
-            if (vinilo.artista == artista)
-                System.out.printf("fila %3d %s - %s - %s %n", i, vinilo.album, vinilo.artista, vinilo.year);
+            if (vinilo.artista.toLowerCase().equals(artista.toLowerCase()))
+                System.out.printf("fila %3d %s - %s - %s %n", i, vinilo.artista, vinilo.album, vinilo.year);
             i++;
         }
 
     }
 
+    // Imprime por pantalla toda la coleccion de vinilos
     public void mostrarColeccion(){
-
         int i = 1;
         for(Vinilo vinilo : vinilos)
             System.out.printf("fila %3d %s - %s - %s %n", i++, vinilo.album, vinilo.artista, vinilo.year);
 
-        System.out.printf("%n");
-
+        System.out.println();
     }
 
-    public int disponibles(){
-        return 100 - totalVinilos();
-    }
+    // Retorna la cantidad de filas disponibles
+    public int disponibles(){return 100 - totalVinilos();}
 
-    public void mostrarDisponibles(){
-        System.out.printf("Queda un total de %d espacios libres %n", disponibles());
-    }
+    // Muestra cuantos espacios quedan disponibles
+    public void mostrarDisponibles(){System.out.printf("Queda un total de %d espacios libres %n%n", disponibles());}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,35 @@
 public class Main {
     public static void main(String[] args){
 
+        // Instancia una coleccion de vinilos llamada myColeccion
         ColeccionVinilos myColeccion = new ColeccionVinilos();
 
+        // Agrega 13 vinilos
         myColeccion.agregarVinilo("Iron Maiden","Iron Maiden","1980");
         myColeccion.agregarVinilo("Iron Maiden", "The Number of the Beast", "1982");
         myColeccion.agregarVinilo("Metallica", "Ride the Lightning", "1984");
         myColeccion.agregarVinilo("Led Zeppelin", "Physical Graffiti", "1975");
         myColeccion.agregarVinilo("Pink Floyd", "Wish You Were Here", "1975");
+        myColeccion.agregarVinilo("Los Enanitos Verdes", "Lamento Boliviano", "1994");
         myColeccion.agregarVinilo("Queen", "News of the World", "1977");
+        myColeccion.agregarVinilo("Charly García", "No Voy en Tren", "1987");
         myColeccion.agregarVinilo("The Beatles", "Abbey Road", "1969");
         myColeccion.agregarVinilo("AC/DC", "Back in Black", "1980");
         myColeccion.agregarVinilo("Nirvana", "Nevermind", "1991");
         myColeccion.agregarVinilo("The Rolling Stones", "Let It Bleed", "1969");
         myColeccion.agregarVinilo("Bob Dylan", "Highway 61 Revisited", "1965");
 
-        myColeccion.mostrarColeccion();
+        // Muestra el estado de la coleccion
         myColeccion.mostrarTotal();
         myColeccion.mostrarDisponibles();
+
+        // Busca un artista con sus vinilos y los imprime por pantalla
+        System.out.println("Buscar el artista Iron Maiden");
+        myColeccion.buscarArtista("Iron Maiden");
         myColeccion.mostrarBusquedaArtista("Iron Maiden");
+
+        // Imprime por pantalla toda la colección
+        myColeccion.mostrarColeccion();
 
     }
 }
